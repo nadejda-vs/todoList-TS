8011f0a153159e27ec96a4a5b275b26b7746ad3d
NadeyaYakatsuk <nadejdagrand27@gmail.com>
2022-07-02 13:10:08 +0300

    start Todolist with TYPESCRIPT

---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,31 @@
+type TasksType={
+   id:number
+    subtitle:string
+    isDone:boolean
+}
+
+type PropsType={
+    title:string
+    tasks:Array<TasksType>
+}
+
+
+
+
+export function ToDoList(props:PropsType) {
+
+    return (
+        <div>
+        <div>{props.title}</div>
+            <input/>
+            <button>Add new task</button>
+            <ul>
+                <li><input type="checkbox" checked={props.tasks[0].isDone} /><span>{props.tasks[0].subtitle}</span></li><li><input type="checkbox" checked={props.tasks[1].isDone} /><span>{props.tasks[1].subtitle}</span></li><li><input type="checkbox" checked={props.tasks[2].isDone} /><span>{props.tasks[2].subtitle}</span></li>
+
+                    </ul>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+        </div>
+    )
+}
