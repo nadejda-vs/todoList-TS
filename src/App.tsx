27b545ca68a7ab0f3ba17c4292d379ea974b25f0
NadeyaYakatsuk <nadejdagrand27@gmail.com>
2022-07-02 13:18:55 +0300

    start Todolist with TYPESCRIPT2

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,24 @@ import './App.css';
 import {ToDoList} from "./components/ToDoList";
 
 function App() {
-    let tasks1=[
-        {id:1,subtitle:"Read a book.",isDone:true},
-        {id:2,subtitle:"Go on a walk with dog",isDone:true},
-        {id:3,subtitle:" Learn Typescript",isDone:false},
-        {id:4,subtitle:" Learn English words",isDone:false},
+    let tasks1 = [
+        {id: 1, subtitle: "Read a book.", isDone: true},
+        {id: 2, subtitle: "Go on a walk with dog", isDone: true},
+        {id: 3, subtitle: " Learn Typescript", isDone: false},
+        {id: 4, subtitle: " Learn English words", isDone: false},
     ]
-    let tasks2=[
-        {id:1,subtitle:"HTML",isDone:true},
-        {id:2,subtitle:"JS",isDone:true},
-        {id:3,subtitle:"HTML",isDone:false},
-        {id:4,subtitle:"TypeScript",isDone:false},
+    let tasks2 = [
+        {id: 1, subtitle: "HTML", isDone: true},
+        {id: 2, subtitle: "JS", isDone: true},
+        {id: 3, subtitle: "HTML", isDone: false},
+        {id: 4, subtitle: "TypeScript", isDone: false},
     ]
-    return ( <>
-         <ToDoList title="What to DO" tasks={ tasks1}/>
-         <ToDoList title="What to learn"tasks={ tasks2} />
-               </>
-          );
+    return (
+        <>
+            <ToDoList title="What to DO" tasks={tasks1}/>
+            <ToDoList title="What to learn" tasks={tasks2}/>
+        </>
+    );
 }
 
 export default App;
